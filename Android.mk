f5019fe2033deeb4f4bfa5e820ec539ba670e99f
Wig C <onlywig@gmail.com>
2017-04-26 00:16:13 +0800

    UART-Test: first initialize.

---
+++ Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := \
+        $(call all-java-files-under, src)
+
+LOCAL_PACKAGE_NAME := UART-Test
+LOCAL_CERTIFICATE := platform
+
+LOCAL_JNI_SHARED_LIBRARIES := serialport
+
+LOCAL_REQUIRED_MODULES := serialport
+
+include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
